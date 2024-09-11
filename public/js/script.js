e3a7fe0c154ffb18d7a3204f975a91adc26c8289
Phattharaphum Kingchai - 6609612160 <144298651+Phattharaphum@users.noreply.github.com>
2024-09-11 17:38:32 +0700

    Delete public directory

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-const inputUser= document.getElementById("userinp")
-const inputpass= document.getElementById("passinp")
-
-inputUser.addEventListener("keydown",function(event){
-    if(event.key==="Enter"){
-        event.preventDefault();
-        inputpass.focus();
-    }
-});
